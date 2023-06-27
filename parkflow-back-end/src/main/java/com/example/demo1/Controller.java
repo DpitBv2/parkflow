7815f8eea353eddb17f47faf1d9b295b7177c7f7
AlexMoto69 <alexmotoasca2006@gmail.com>
2023-06-27 20:42:05 +0300

    init of back end

---
+++ parkflow-back-end/src/main/java/com/example/demo1/Controller.java
@@ -0,0 +1,12 @@
+package com.example.demo1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping(path="/")
+    public String message(){
+        return "Pomohaci";
+    }
+}

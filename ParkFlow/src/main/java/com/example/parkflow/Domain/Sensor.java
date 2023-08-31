c7be51118c6d02fca37749919f6cf517170fe102
AlexMoto69 <alexmotoasca2006@gmail.com>
2023-08-31 18:12:26 +0300

    Added sensor entity

---
+++ ParkFlow/src/main/java/com/example/parkflow/Domain/Sensor.java
@@ -0,0 +1,93 @@
+package com.example.parkflow.Domain;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Sensor {
+    private Long id;
+    private Long hubId;
+    private Boolean state;
+    private Boolean currentState;
+    private double latitude;
+    private double longitude;
+    private Timestamp createdAt;
+
+    public Sensor(Long id, Long hubId, Boolean state, Boolean currentState,
+                  double latitude, double longitude, Timestamp createdAt) {
+        this.id = id;
+        this.hubId = hubId;
+        this.state = state;
+        this.currentState = currentState;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.createdAt = createdAt;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getHubId() {
+        return hubId;
+    }
+
+    public void setHubId(Long hubId) {
+        this.hubId = hubId;
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+
+    public Boolean getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(Boolean currentState) {
+        this.currentState = currentState;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Sensor{" +
+                "id='" + id + '\'' +
+                ", hubId='" + hubId + '\'' +
+                ", state='" + state + '\'' +
+                ", currentState='" + currentState + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}

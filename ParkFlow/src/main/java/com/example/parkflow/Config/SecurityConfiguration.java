b0eca137cc03a2c41209d2855d177d9b873915a8
AlexMoto69 <alexmotoasca2006@gmail.com>
2023-09-16 23:10:13 +0300

    push1000

--- ParkFlow/src/main/java/com/example/parkflow/Config/SecurityConfiguration.java
+++ ParkFlow/src/main/java/com/example/parkflow/Config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.example.parkflow.Security.AuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(
